dc1ebb7452b813879e940087816758440294c0da
JunR <68640939+lee-JunR@users.noreply.github.com>
2024-08-07 01:34:31 +0900

    feat: 뉴스 가사 요약하는 엔드포인트 추가

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
+import random
+
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 import os
+import requests
+from bs4 import BeautifulSoup as bs
 
 from ocr import ocr_with_clova
 from parse import parse_ocr_data
 from query_generator import generate_sql_query
 from sql_executor import execute_sql_query
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains.combine_documents.stuff import StuffDocumentsChain
+from langchain.chains.llm import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
 
 # .env 파일의 경로를 명시적으로 설정
 dotenv_path = os.path.join(os.path.dirname(__file__), 'env/.env')
@@ -13,13 +23,83 @@ load_dotenv(dotenv_path=dotenv_path)
 
 app = Flask(__name__)
 
+
 def load_environment_variables():
-    required_vars = ['DB_USER', 'DB_PASSWORD', 'DB_HOST', 'DB_PORT', 'DB_NAME', 'OPENAI_API_KEY', 'CLOVA_API_KEY', 'CLOVA_ENDPOINT', 'LANGCHAIN_ENDPOINT']
+    required_vars = ['DB_USER', 'DB_PASSWORD', 'DB_HOST', 'DB_PORT', 'DB_NAME', 'OPENAI_API_KEY', 'CLOVA_API_KEY',
+                     'CLOVA_ENDPOINT', 'LANGCHAIN_ENDPOINT']
     for var in required_vars:
         if not os.getenv(var):
             raise ValueError(f"Environment variable {var} is missing")
 
+
 load_environment_variables()
+
+# LLM 객체 생성
+llm = ChatOpenAI(
+    api_key=os.getenv('OPENAI_API_KEY'),
+    temperature=1.0,
+    max_tokens=2048,
+    model_name="gpt-4o-mini"
+)
+
+
+def get_article_link(search_url, n):
+    # USER_AGENT 설정
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+
+    # 네이버 뉴스 검색 결과 페이지 로드
+    page = requests.get(search_url, headers=headers)
+    soup = bs(page.text, "html.parser")
+
+    # 기사 링크 가져오기
+    elements = soup.select(".news_tit")
+
+    # 기사 링크가 n번째 요소를 선택
+    if n < 1 or n > len(elements):
+        raise ValueError("주어진 n 값이 유효하지 않습니다.")
+
+    article_element = elements[n - 1]
+    article_url = article_element.get('href')
+
+    return article_url
+
+
+def summarize_url(url):
+    # 웹 페이지 로드
+    loader = WebBaseLoader(url)
+    text = loader.load_and_split()
+
+    # 내용 추출
+    load_content = text[0].page_content
+
+    # 텍스트 분할
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len,
+        is_separator_regex=False,
+    )
+
+    texts = text_splitter.create_documents([load_content])
+
+    # 프롬프트 정의
+    prompt_template = """당신은 시사 상식 전문가입니다. 내용을 말투는 캐주얼한 톤앤 매너와 이모티콘을 추가해서 시사 상식을 잘 모르는 사람도 알 수 있게 쉬운 설명으로 3줄로 말해주세요:
+    "{text}"
+   """
+    prompt = PromptTemplate.from_template(prompt_template)
+
+    # LLM 체인 정의
+    llm_chain = LLMChain(llm=llm, prompt=prompt)
+
+    # StuffDocumentsChain 정의
+    stuff_chain = StuffDocumentsChain(llm_chain=llm_chain, document_variable_name="text")
+
+    # 요약 결과 출력
+    summary = stuff_chain.run(texts)
+    return summary
 
 
 @app.route('/chatbot/', methods=['POST'])
@@ -39,6 +119,7 @@ def query_expenses():
         return jsonify({"query": generated_query, "response": result})
     except Exception as e:
         return jsonify({"error": f"서버 오류 발생: {e}"}), 500
+
 
 @app.route('/parse-ocr/', methods=['POST'])
 def parse_ocr():
@@ -73,6 +154,29 @@ def parse_ocr():
     except Exception as e:
         print(f"서버 오류 발생: {e}")
         return jsonify({"error": f"서버 오류 발생: {e}"}), 500
+
+
+@app.route('/summarize-news/', methods=['GET'])
+def summarize_news():
+    """뉴스 기사 링크를 요약"""
+    search_url = "https://search.naver.com/search.naver?where=news&ie=utf8&sm=nws_hty&query=kb"
+    n = random.randint(1, 5)  # 1~5 사이의 정수
+
+    try:
+        article_url = get_article_link(search_url, n)
+        print(f"선택된 기사 링크: {article_url}")
+
+        # 요약 결과 출력
+        summary = summarize_url(article_url)
+        print(f"\n요약내용 : {summary}")
+
+        return jsonify({"url": article_url, "summary": summary})
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
+    except Exception as e:
+        print(f"서버 오류 발생: {e}")
+        return jsonify({"error": f"서버 오류 발생: {e}"}), 500
+
 
 if __name__ == "__main__":
     app.run(debug=True)
